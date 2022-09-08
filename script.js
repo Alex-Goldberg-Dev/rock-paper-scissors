1716e4cfd72a75e6a115acfdf5fa455fe1a5d9f9
Alex-Goldberg-Dev <alex.h.goldberg16@gmail.com>
2022-09-08 15:08:38 -0400

    add function playRound

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ function getComputerChoice() {
     // generate random number, multiply by 3, and round to an integer
     const randNum = Math.floor((Math.random() * 3) + 1);
     // check the number and return ‘Rock’, ‘Paper’ or ‘Scissors’
-    if (randNum === 1) return 'Rock';
-    else if (randNum === 2) return 'Paper';
-    else return 'Scissors';
+    if (randNum === 1) return 'rock';
+    else if (randNum === 2) return 'paper';
+    else return 'scissors';
 }
 
 function playRound(playerSelection, computerSelection) {
     // compare the choices and return the winner accordingly
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') return 'You Win! Rock beats Scissors';
+    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') return 'You Win! Scissors beats Paper';
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') return 'You Win! Paper beats Rock';
+    else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') return 'You Lose! Paper beats Rock';
+    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') return 'You Lose! Rock beats Scissors';
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') return 'You Lose! Scissors beats Paper';
+    else return 'Draw!'
   }
 
   const playerSelection = "rock";
@@ -17,19 +24,6 @@ function playRound(playerSelection, computerSelection) {
 
 
 /*
-Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and
-computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on,
-so let’s test this function by using console.log to see the results:
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports
 a winner or loser at the end.
 Remember loops? This is a great opportunity to use one to play those five rounds:

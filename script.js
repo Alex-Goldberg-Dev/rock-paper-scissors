d539320f28160daf2d897d9eb09412bcdd6cafee
Alex-Goldberg-Dev <alex.h.goldberg16@gmail.com>
2022-09-08 14:59:51 -0400

    set up next step

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ function getComputerChoice() {
     else return 'Scissors';
 }
 
+function playRound(playerSelection, computerSelection) {
+    // compare the choices and return the winner accordingly
+  }
+
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
 
 /*
-Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return
-either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play.
-Tip: use the console to make sure this is returning the expected output before moving to the next step!
 Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and
 computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
